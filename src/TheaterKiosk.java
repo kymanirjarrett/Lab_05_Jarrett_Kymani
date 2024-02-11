6a5ee957e6bff9718f982041c519c37c8086abf1
kymanirjarrett <krj.kymani13@gmail.com>
2024-02-10 22:05:15 -0500

    Task 2 Complete

--- src/TheaterKiosk.java
+++ src/TheaterKiosk.java
@@ -4,6 +4,21 @@ public class TheaterKiosk
 {
     public static void main(String[] args)
     {
+        Scanner in = new Scanner(System.in);
+        String trash;
 
+        int age;
+
+        System.out.print("Enter your age: ");
+        age = in.nextInt();
+
+        if(age >= 21)
+        {
+            System.out.println("You get a paper wristband!");
+        }
+        else
+        {
+            System.out.println("You are too young. Come back later.");
+        }
     }
 }
